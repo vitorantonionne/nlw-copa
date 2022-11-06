1f0a8ea9491f69bef4f589f70a2845977381c98d
Vitor Antonionne <114239671+vitorantonionne@users.noreply.github.com>
2022-11-06 17:47:12 -0300

    efeitos

--- main.js
+++ main.js
@@ -19,15 +19,9 @@ function createCard(date , day, games) {
             </ul>
         </div>
     </div>
-    `
+    `  
 } 
-document.querySelector("#app").innerHTML = `
-    <header>
-        <img src="imagens-paises/logo.svg" alt="logo nwl copa">
-        </header>
-    <main id="cards">   
-    ${createCard('24/11', 'quinta',createGame("brasil", "16:00", "servia"))}
-    ${createCard('28/11', 'segunda',createGame("suiça", "13:00", "brasil"))}
-    ${createCard('02/12', 'sexta',createGame("brasil", "16:00", "camaroes"))}  
-    </main>
-`
+document.querySelector("#cards").innerHTML = 
+createCard('24/11', 'quinta',createGame("brasil", "16:00", "servia"))+
+createCard('28/11', 'segunda',createGame("suiça", "13:00", "brasil"))+
+createCard('02/12', 'sexta',createGame("brasil", "16:00", "camaroes"))
